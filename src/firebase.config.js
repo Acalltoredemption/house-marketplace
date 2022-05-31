bd2e05909473b90024df664e4cc1a67565c89035
Stino216 <acalltoredemption@gmail.com>
2022-05-31 14:05:37 -0400

    routing and navbar styling

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import {getFirestore} from 'firebase'
+import { initializeApp } from "firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnXOhSsVM4sy-ZzyQ1IF2zw_3sRDck94Y",
+  authDomain: "house-marketplace-73b3b.firebaseapp.com",
+  projectId: "house-marketplace-73b3b",
+  storageBucket: "house-marketplace-73b3b.appspot.com",
+  messagingSenderId: "71028694936",
+  appId: "1:71028694936:web:5ecd1e2bd63a7213e815ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
